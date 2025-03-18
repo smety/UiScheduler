a3b0be343f88889942fffec41e7146070ce9acd5
TS <smetka.tomas90@gmail.com>
2025-03-18 13:54:35 +0100

    Fix format by pint

--- Modules/UiScheduler/Http/Controllers/UiSchedulerController.php
+++ Modules/UiScheduler/Http/Controllers/UiSchedulerController.php
@@ -3,7 +3,6 @@
 namespace Modules\UiScheduler\Http\Controllers;
 
 use Illuminate\Contracts\Support\Renderable;
-use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
 class UiSchedulerController extends Controller
